31cbd0c18991a3a5dd60b1c5403230227ded214e
aguilanbon <aguilanbon@gmail.com>
2022-05-14 20:33:34 +0800

    initial files

---
+++ app.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios'
+
+// const fetchWeather = async () => {
+//     let response = await axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=013379e640835e59f4a4b5b4b08e6b78')
+//     console.log(response.data);
+// }
+
+// fetchWeather()
